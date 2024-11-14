ece29905cb9648727c18c6ca8f0974cc2dba5e3b
suresh362 <727723eucy059@skcet.ac.in>
2024-11-14 20:21:33 +0530

    Add files via upload

---
+++ reactportalcoding-main/Day9/ce/Singer.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+const Singers = () => {
+ return (
+ <div>
+ <h2>Singers</h2>
+ <ul>
+ <li>Singer 1 (Year)</li>
+ <li>Singer 2 (Year)</li>
+ </ul>
+ </div>
+ );
+};
+export default Singers;
